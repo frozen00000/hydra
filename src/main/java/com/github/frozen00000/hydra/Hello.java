6af95127d73ad49ef241d3ce58b02f900d3094a0
timurlevadny <frozen00000>
2020-04-02 22:24:03 -0700

    Add build info.

--- src/main/java/com/github/frozen00000/hydra/Hello.java
+++ src/main/java/com/github/frozen00000/hydra/Hello.java
@@ -1,16 +1,27 @@
 package com.github.frozen00000.hydra;
 
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class Hello {
+
+	private final BuildProperties buildProperties;
 
 	@GetMapping("/hello")
 	public String hello() {
-		return "Hello v2";
+		return "Hello";
+	}
+
+	@GetMapping("/version")
+	public String version() {
+		return buildProperties.getVersion();
 	}
 
 }
